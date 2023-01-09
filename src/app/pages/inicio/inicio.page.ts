070a75f9c239bf31c3d92a240748b832002e07e3
micaelavera <micavera2010@hotmail.com>
2023-01-09 13:35:04 -0300

    add ion-list and ion-items

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string,
+  name: string,
+  redirectTo: string
+}
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -7,6 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  componentes : Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo:'/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo:'/alert'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
