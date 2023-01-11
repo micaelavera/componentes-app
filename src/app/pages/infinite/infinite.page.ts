0767d98741a0120d9f51c18db1e1ff392c6d012c
Micaela <micaelavera@telepeajeplus.com>
2023-01-11 08:51:25 -0300

    add data and infinite scroll

--- src/app/pages/infinite/infinite.page.ts
+++ src/app/pages/infinite/infinite.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-infinite',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfinitePage implements OnInit {
 
+  data : any[] = Array(20);
+
+  @ViewChild(IonInfiniteScroll, {static: false}) infiniteScroll! : IonInfiniteScroll;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loadData(event:any){
+    //console.log(event);
+
+    setTimeout(() => {
+
+      if (this.data.length > 50){
+        this.infiniteScroll.complete();
+        this.infiniteScroll.disabled = true;
+        return;
+      }
+      const nuevoArray = Array(20);
+      this.data.push(...nuevoArray);
+
+      //event.target.complete();
+      this.infiniteScroll.complete();
+    }, 1500);
+
   }
 
 }
