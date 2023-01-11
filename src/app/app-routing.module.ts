d771b1254d165ef81de4c49aca96b24f4b5c5dbc
Micaela <micaelavera@telepeajeplus.com>
2023-01-11 18:29:22 -0300

    add menu to app-componnent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,8 @@ const routes: Routes = [
   {
     path: 'avatar',
     loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
-  },  {
+  },
+  {
     path: 'button',
     loadChildren: () => import('./pages/button/button.module').then( m => m.ButtonPageModule)
   },
