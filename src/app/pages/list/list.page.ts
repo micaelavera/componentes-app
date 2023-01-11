49db1fe794e6a5ab35587e76519634d67c4abd40
Micaela <micaelavera@telepeajeplus.com>
2023-01-11 15:47:55 -0300

    add items sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService } from '../../services/data.service';
 
@@ -10,13 +11,28 @@ import { DataService } from '../../services/data.service';
 export class ListPage implements OnInit {
 
   usuarios!: Observable<any>;
+  @ViewChild(IonList) ionList! : IonList;
+
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-  
   //this.dataService.getUsuarios().subscribe(console.log);
   this.usuarios = this.dataService.getUsuarios();
-  
+  }
+
+  favorite(user:any){
+    console.log('favorite', user);
+    this.ionList.closeSlidingItems();
+  }
+
+  share(user:any){
+    console.log('share', user);
+    this.ionList.closeSlidingItems();
+  }
+
+ delete(user:any){
+    console.log('delete', user.name);
+    this.ionList.closeSlidingItems();
   }
 
 }
