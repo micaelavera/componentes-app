0850afc38e17a218abecf05cfedd7abc827d8f9a
Micaela <micaelavera@telepeajeplus.com>
2023-01-11 16:49:38 -0300

    add list-reorder and doReorder function

--- src/app/pages/list-reorder/list-reorder.page.ts
+++ src/app/pages/list-reorder/list-reorder.page.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListReorderPage implements OnInit {
 
+  personajes : string[] = ['Aquaman', 'Superman','Batman','Mujer Maravilla', 'Flash'];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doReorder( event:any ){
+    console.log(event);
+
+    const itemMover = this.personajes.splice(event.detail.from, 1)[0];
+    this.personajes.splice(event.detail.to, 0, itemMover);
+    event.detail.complete();
+
+    console.log(this.personajes);
   }
 
 }
