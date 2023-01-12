82e5ca26f653df1b33693e6b9abcaeeb0342a959
Micaela <micaelavera@telepeajeplus.com>
2023-01-12 15:38:35 -0300

    import example for slides: icons, json and styles

--- src/app/pages/slides/slides.page.ts
+++ src/app/pages/slides/slides.page.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidesPage implements OnInit {
 
+  slides: { img: string, titulo: string, desc: string }[] = [
+    {
+      img: '/assets/slides/photos.svg',
+      titulo: 'Comparte Fotos',
+      desc: 'Mira y comparte increíbles fotos de todo el mundo'
+    },
+    {
+      img: '/assets/slides/music-player-2.svg',
+      titulo: 'Escucha Música',
+      desc: 'Toda tu música favorita está aquí'
+    },
+    {
+      img: '/assets/slides/calendar.svg',
+      titulo: 'Nunca olvides nada',
+      desc: 'El mejor calendario del mundo a tu disposición'
+    },
+    {
+      img: '/assets/slides/placeholder-1.svg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(){
+    
   }
 
 }
