f1c1dbe1f880b41e4aa457b815ec1ebdf11a7536
Micaela <micaelavera@telepeajeplus.com>
2023-01-12 11:02:35 -0300

    add getAlbums() service and show items

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  albums: any[] = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getAlbums().subscribe(albums =>{
+      console.log(albums)
+      this.albums = albums;
+    })
   }
 
   onSearchChange( event:any ){
